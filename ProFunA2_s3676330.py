747ff3f504afaaf0a637439b9d6aec9f1ade1a31
Dang Quang <79730898+Rus268@users.noreply.github.com>
2023-12-20 11:23:44 +0700

    Update the class to meet the Credit level
    Adding additional exception handle feature.

--- ProFunA2_s3676330.py
+++ ProFunA2_s3676330.py
@@ -1,6 +1,10 @@
 """
- This program is a retail management system that allows cashier to place an order for a customer, add/update products and prices, and display existing customers and products, etc.
-"""
+ This program is a retail management system that allows cashier to place an order for a customer, 
+ add/update products and prices, and display existing customers and products, etc.
+
+ - 14/12/2023: Create program acording to the requirement for PASS level in week 7.
+ 
+ """
 import sys
 import os
 
@@ -10,47 +14,45 @@ class Customer:
     def __init__(self, id:str, name:str, value:float = 0.00):
         self.id = id
         self.name = name
-        self.value = float(value) # Ensuring value is store as a float
+        self.value = value
     # Calculate the discount which return (0,price) - where the first value is discount rate and second is input price.
     # This will be the super method and will have more subclass implement
     def get_discount(self,price):
         return (0,price)
-    
-    @property
-    def get_id(self): # Method to get the customer ID
-        return self.id
-    
-    @property
-    def get_name(self): # Method to get the customer name
-        return self.name
-    
-    @property
-    def get_value(self): # method to get the customer total money spend to date
-        return self.value
-    
-    # Print the class Customer attributes and the discount rate associated:
+    # Return the class information and the discount information.
     def display_info(self):
-        return print(f"ID: {self.id}\nName: {self.name}\nValue: {self.value}\nDiscount Rate: 0")
+        return f"ID: {self.id}\nName: {self.name}\nValue: {self.value}\nDiscount Rate: 0"
+    
 # Create a new member class that inherited it attributes from customer
 class Member(Customer):
-    discount_rate = 0.05 # initialise a class inherited variable
-
+    discount_rate = 0.05 # Initialise a class inherited variable
     # Initialise the class member and inherited the attributes from customer class.
     def __init__(self,id, name:str, value:float = 0.00):
-        super().__init__(id,name,value)
+        # Initialise the class throught initialising the parent class variable.
+        super().__init__(id,name,value) 
+    # Update the get_discount method with new calculation
+    def get_discount(self, price:float):
+        """
+        This function will calculate the discount for the member
 
-    def get_discount(self, price:float): # Update the get_discount method with new calculation
+        Input:
+        - price: the price of the product
+
+        Output:
+        - return a tuple contain the discount rate and the new price
+        """ 
         new_price = price * (1 - self.discount_rate)
         return (self.discount_rate, new_price)
-    
-    def display_info(self): # Update new information in the display_infor class
-        print(f"ID: {self.id}\nName: {self.name}\nValue: {self.value}\nDiscount Rate: {self.discount_rate}")
-    
+     # Update new information in the display_infor class
+    def display_info(self):
+        """This function will print the information of the member"""
+        return f"ID: {self.id}\nName: {self.name}\nValue: {self.value}\nDiscount Rate: {self.discount_rate}"
     # Create a new class method to update the flat discount rate
     # I will be using @classmethod decorator to ensure the method is a class method
     @classmethod
-    def set_rate(member_cls, new_rate: float): # Create a new class method to update the flat discount rate
-        member_cls.discount_rate = new_rate
+    def set_rate(cls, new_rate: float): # Create a new class method to update the flat discount rate
+        cls.discount_rate = new_rate
+
 # Create a new vip member class that inherited it attributes from customer
 class VipMember(Customer):
     # Initialise the two discount rate and threshold for the class
@@ -109,53 +111,52 @@ class Product:
     def __init__(self, id, name:str, price:float, stock:int):
         self.id = id
         self.name = name
-        self.price = float(price) # Ensuring price is store as a float
-        self.stock = int(stock) # Ensuring stock is store as an integer
-    
-    def get_id(self):
-        return self.id
+        self.price = price # Ensuring price is store as a float
+        self.stock = stock # Ensuring stock is store as an integer
 
-    def get_name(self):
-        return self.name
-    
-    def get_price(self):
-        return self.price
-    
-    def get_stock(self):
-        return self.stock
      # Update the price of the product
     def set_price(self, new_price:float):
+        """
+        This function will update the price of the product
+        
+        Input:
+        - new_price: the new price of the product
+        """
         self.price = float(new_price)
     # Update the stock of the product
-    def set_stock(self, new_stock:int): 
+    def set_stock(self, new_stock:int):
+        """
+        This function will update the stock of the product
+        
+        Input:
+        - new_stock: the new stock of the product
+        """
         self.stock = int(new_stock)
     # Print the product information
     def display_info(self):
-        print(f"ID: {self.id}\nName: {self.name}\nPrice: {self.price}\nStock: {self.stock}")
+        return f"ID: {self.id}\nName: {self.name}\nPrice: {self.price}\nStock: {self.stock}"
+    
 class Order:
     def __init__(self, customer:Customer, product:Product, quantity:int):
         self.customer = customer # We will be using customer id as it ensure we are reference to the correct customer everytime
         self.product = product # We will be using product id to ensure we are reference to to the correct product everytime
         self.quantity = quantity
-    # I will be using @property decorator to ensure the attribute is read only for getter method
-    @property
-    def get_customer(self):
-        return self.customer
-    @property
-    def get_product(self):
-        return self.product
-    @property
-    def get_quantity(self):
-        return self.quantity
-    
+
     def update_product_stock(self):
         """This function will update the stock of the product after the order is placed"""
         # For this week requirement I will ignored the case there the value is negative.
         try:
+            # Calculate the new stock
             self.product.stock = self.product.stock - self.quantity
+            # Update the product stock
+            self.product.set_stock(self.product.stock)
         except ValueError:
             raise ValueError("Invalid quantity")
-
+        
+    def update_customer_value(self):
+        """This function will update the customer value after the order is placed"""
+        # Calculate the new value and update it.
+        self.customer.value = self.customer.value + (self.product.price * self.quantity)
     def list_order(self):
         """This function will list the order"""
         print(f"Customer ID: {self.customer_id}\nProduct ID: {self.product_id}\nQuantity: {self.quantity}")
@@ -177,34 +178,60 @@ class Record:
             with open(self.customer_file, "r") as f:
                 line = f.readline()
                 while line:
-                    id, name, discount_rate, value = line.strip().split(',')
-                    # I will be using if statement to determine the type of customer
-                    # I will be using the first letter of the id to determine the type of customer to create the correct object
-                    if "C" in id:
+                    elements = line.strip().split(',')
+                    # Validate the customer record
+                    if len(elements) == 4:
+                         id, name, discount_rate, value = elements
+                    else:
+                        raise ValueError(f"Invalid customer record {elements}")
+                    # Validate the customer id and create the customer object
+                    if id.startswith("C"):
                         customer_id = Customer (id, name, value)
                         self.customer_list.append(customer_id)
                         line = f.readline()
-                    elif "M" in id:
+                    elif id.startswith("M"):
                         customer_id = Member (id, name, value)
                         self.customer_list.append(customer_id)
                         line = f.readline()
-                    elif "V" in id:
+                    elif id.startswith("V"):
                         customer_id = VipMember (id, name, value)
                         customer_id.set_rate("rate_1",discount_rate)
                         self.customer_list.append(customer_id)
                         line = f.readline()
+                    else:
+                        raise ValueError(f"Invalid customer id {id}")
+                    # Validate that the customer id is unique
+                    if id in [customer.id for customer in self.customer_list]:
+                        raise ValueError(f"Duplicate customer id {id}")
+                    if value in [customer.value for customer in self.customer_list < 0]:
+                        raise ValueError(f"Invalid customer value {value}")
         except FileNotFoundError:
             raise FileNotFoundError(f'File {self.customer_file} is missing!')
     
     def read_product(self):
+        """This function will read the product file and import into the record """
         try:
-            """This function will read the product file and import into the record """
             with open(self.product_file,"r") as f:
                 line = f.readline()
                 while line:
-                    item_id, name, price, stock = line.strip().split(',')
+                    elements = line.strip().split(',')
+                    # Validate the product record
+                    if len(elements) == 4:
+                        item_id, name, price, stock = elements
+                    else:
+                        raise ValueError(f"Invalid product record {elements}")
+                    # Validate the product id and create the product object
+                    if not item_id.startswith("P"):
+                        raise ValueError(f"Invalid product id {item_id}")
+                    if price < 0:
+                        raise ValueError(f"Invalid product price {price}")
+                    if stock < 0:
+                        raise ValueError(f"Invalid product stock {stock}")
                     product_id = Product(item_id, name, price, stock)
                     self.product_list.append(product_id) # Add the new product into the product id
+                    # Check if the product id is unique
+                    if id in [product.get_id for product in self.product_list]:
+                        raise ValueError(f"Duplicate product id {id}")
                     line = f.readline() # Read the next line
         except FileNotFoundError:
             raise FileNotFoundError(f'File {self.product_file} is missing!')
@@ -314,46 +341,78 @@ def menu(Data:Record):
         print()
 
 
-def get_info(Data:Record):
+def get_info(data:Record):
     """
     This function will take information from the client
     
     Input:
-    - Data: the record object that contain the customer and product list
+    - Data: the main record object that contain the customer and product list
     """
     name = input("Please enter the customer name [e.g. Loki]: ")
     # We will be creating a new customer attributes to track the customer details
-    customer = Data.find_customer("name",name)
+    customer = data.find_customer("name", name)
     print()
-    product = input("Please enter the product name [e.g. Apple]: ")
-    product = Data.find_product("name",product)
+    product = get_product_info(data)
     print()
-    quantity = int(input("Please enter the quantity [e.g. 10]: "))
+    quantity = get_product_quantity(product)
     print()
     # If the customer does not exit, we will be creating a new customer and check if they want a membership
-    if customer == None:
-        membership = input("This is a new customer. Does the customer want to have a membership [e.g. enter y or n]: ")
-        if membership == "y":
-            print("We have two type of membership: V for VIP and M for Member")
-            type = input("Please enter the membership type [e.g. V or M]: ")
-            if type == "V":
-                id = generate_new_customer_id(type,Data)
-                customer = VipMember(id,name,0.00)
-            elif type == "M":
-                id = generate_new_customer_id(type,Data)
-                customer = Member(id,name,0.00)
-        elif membership == "n":
-            id = generate_new_customer_id("C",Data)
-            customer = Customer(id,name,0.00)
-        else:
-            raise ValueError("Invalid input")
-        # Add the new customer into the record
-        Data.add_customer(customer)
-    if product == None:
-        # TODO: handle when product does not exit in next version
-        raise ValueError("Invalid product")
-    
+    if customer is None:
+        member_type = get_membership()
+        id = generate_new_customer_id(member_type, data)
+        new_customer = Customer(id, name)
+        data.add_customer(new_customer)
     return customer, product, quantity
+
+def get_membership():
+    """This function will take the membership information from the client"""
+    while True:
+        _membership = input("This is a new customer. \
+                            Does the customer want to have a membership [e.g. enter y or n]: ")
+        if _membership == "y":
+            print("We have two type of membership: V for VIP and M for Member")
+            while True:
+                _type = input("Please enter the membership type [e.g. V or M]: ")
+                if _type == "V":
+                    return "V"
+                elif _type == "M":
+                    return "M"
+                print("Invalid input. Please enter V or M")
+        elif _membership == "n":
+            return "C"
+        print('Invalid input. Please enter y or n')
+
+def get_product_info(data: Record):
+    """
+    This function will take information from the client
+    
+    Input:
+    - Data: the main record object that contain the customer and product list
+    """
+    while True:
+        name = input("Please enter the product name [e.g. Apple]: ")
+        # We will be creating a new customer attributes to track the customer details
+        _product = data.find_product("name",name)
+        if _product is not None:
+            return _product
+
+def get_product_quantity(item:Product):
+    """
+    This function will take the product quantity from the client
+    """
+    stock = item.stock
+    while True:
+        try:
+            _quantity = int(input("Please enter the quantity [e.g. 10]: "))
+            if _quantity < 0:
+                print ("Invalid input, please try again")
+            elif _quantity > stock:
+                print("Insufficient stock, please try again")
+            else:
+                return _quantity
+        except ValueError:
+            print("Invalid input, please try again")
+            continue        
 
 def generate_new_customer_id(type:str, data:Record):
     """
@@ -413,8 +472,8 @@ def main():
     try:
         SysRecord.read_customer()
         SysRecord.read_product()
-    except FileNotFoundError as e:
-        print(f"{e}")
+    except FileNotFoundError as _e:
+        print(f"{_e}")
         sys.exit("Please check the file path and try again!")
     # Run the menu
     menu(SysRecord)
@@ -422,8 +481,8 @@ def main():
 
 if __name__ == "__main__":
     try:
-     main()
-     # Exit the program gracefully when sys.exit() is called. This is to ensure consistent behaviour across IDE or terminal
+       main()
+    # Exit the program gracefully when sys.exit() is called
+    # This is to ensure consistent behaviour across IDE or terminal
     except SystemExit as e:
         print(f"{e}")
-
